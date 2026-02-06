3252e257b5ba0f8441d1a039bec122325cefffbc
Fernando Asato <fp.asato.dev@gmail.com>
2026-02-05 21:55:20 -0300

    numeros negativos

--- tabuada.py
+++ tabuada.py
@@ -5,15 +5,20 @@
 while True: 
     n1 = int(input('Digite um número: '))
     n2= int(input('Até onde a tabuada vai: '))
-    for i in range(n2):
-        i+=1
-        print(f'{n1} x {i} = {n1 * i}')
-    
 
-    continuar = input('Quer geraar outra tabuada? \n[S]Sim\nQualquer outra tecla para não\n').strip().lower()
-
-    if continuar == 's':
+    if n1 <= 0 or n2 <= 0:
+        print('Números negatiivos não são permitidos!!')
         continue
     else:
-        print("Fechando calculadora...")
-        break
+        for i in range(n2):
+            i+=1
+            print(f'{n1} x {i} = {n1 * i}')
+        
+
+        continuar = input('Quer geraar outra tabuada? \n[S]Sim\nQualquer outra tecla para não\n').strip().lower()
+
+        if continuar == 's':
+            continue
+        else:
+            print("Fechando calculadora...")
+            break
