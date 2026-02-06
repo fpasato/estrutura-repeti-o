41036db37f55d0549af8a1d8306ff85ad47318fe
Fernando Asato <fp.asato.dev@gmail.com>
2026-02-05 21:46:53 -0300

    tabuada

---
+++ atividade.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+
+while True: 
+    n1 = int(input('Digite um número: '))
+    n2= int(input('Até onde a tabuada vai: '))
+    for i in range(n2):
+        i+=1
+        print(f'{n1} x {i} = {n1 * i}')
+    
+
+    continuar = input('Quer geraar outra tabuada? \n[S]sim\nQualquer outra tecla para não\n').strip().lower()
+
+    if continuar == 's':
+        continue
+    else:
+        print("Fechando calculadora...")
+        break
